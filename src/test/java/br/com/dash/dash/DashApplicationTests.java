db0caa1d35266656aa87b417694a7b9ed390f561
marcusv614 <marcus.pinheiro@super.ufam.edu.br>
2025-11-26 19:03:18 -0400

    Commit de deploy do projeto no render

--- src/test/java/br/com/dash/dash/DashApplicationTests.java
+++ src/test/java/br/com/dash/dash/DashApplicationTests.java
@@ -3,8 +3,10 @@ package br.com.dash.dash;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DashApplicationTests {
+import br.com.dash.app.DashApplication;
+
+@SpringBootTest(classes = DashApplication.class)
+public class DashApplicationTests {
 
 	@Test
 	void contextLoads() {
