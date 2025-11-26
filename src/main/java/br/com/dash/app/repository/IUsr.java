e010cd60dacb36b825523eb5497e508698f07c5f
marcusv614 <marcus.pinheiro@super.ufam.edu.br>
2025-11-26 19:49:31 -0400

    Commit de deploy do projeto no render tentativa 4

--- src/main/java/br/com/dash/app/repository/IUsr.java
+++ src/main/java/br/com/dash/app/repository/IUsr.java
@@ -1,11 +1,13 @@
 package br.com.dash.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.dash.app.entity.Usr;
 
 public interface IUsr extends JpaRepository<Usr, Integer> {
-	Usr findByUsername(String username);
+	List<Usr> findByEmail(String email);
 	/*
 	 * Conhecido também como DAO (Data Acess Object) É a camada que acessa o banco
 	 * de dados
